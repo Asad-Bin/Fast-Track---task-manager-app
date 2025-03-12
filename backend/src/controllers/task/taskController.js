8fc999646f39892bc9bf90ebdc704cdd1a2a7d9c
Asad Bin Saber <mdasad.binsaber@gmail.com>
2025-03-13 03:33:43 +0600

    Task system: Added functions for task 'update' & 'delete'

--- backend/src/controllers/task/taskController.js
+++ backend/src/controllers/task/taskController.js
@@ -85,3 +85,71 @@ export const getTask = asyncHandler(async (req, res) => {
         ;
     }
 });
+
+export const updateTask = asyncHandler(async (req, res) => {
+    try{
+        const userId = req.user._id;
+
+        const { task_id } = req.params;
+        const { title, description, dueDate, priority, status, completed } = req.body;
+
+        if(!task_id){
+            return res.status(400).json({ message: "Please provide a task id" });
+        }
+
+        const task = await TaskModel.findById(task_id);
+
+        if(!task){
+            return res.status(404).json({ message:"Task not found!" });
+        }
+
+        // check if the user is the owner of the task
+        if(!task.user.equals(userId)){
+            return res.status(401).json({ message:"Not Authorized!" });
+        }
+
+        task.title = title || task.title;
+        task.description = description || task.description;
+        task.dueDate = dueDate || task.dueDate;
+        task.priority = priority || task.priority;
+        task.status = status || task.status;
+        task.completed = completed || task.completed;
+        
+        await task.save();
+
+        return res.status(200).json(task);
+    } catch(error){
+        console.log("Error in updateTask: ", error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+export const deleteTask = asyncHandler(async (req, res) => {
+    try{
+        const userId = req.user._id;
+
+        const { task_id } = req.params;
+
+        if(!task_id){
+            return res.status(400).json({ message: "Please provide a task id" });
+        }
+
+        const task = await TaskModel.findById(task_id);
+
+        if(!task){
+            return res.status(404).json({ message:"Task not found!" });
+        }
+
+        // check if the user is the owner of the task
+        if(!task.user.equals(userId)){
+            return res.status(401).json({ message:"Not Authorized!" });
+        }
+
+        await TaskModel.findByIdAndDelete(task_id);
+
+        return res.status(200).json({ message: "Task deleted successfully." });
+    } catch(error){
+        console.log("Error in updateTask: ", error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
--- backend/src/routes/tasksRoutes.js
+++ backend/src/routes/tasksRoutes.js
@@ -1,12 +1,14 @@
 
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { createTask, getTask, getTasks } from "../controllers/task/taskController.js";
+import { createTask, deleteTask, getTask, getTasks, updateTask } from "../controllers/task/taskController.js";
 
 const router = express.Router();
 
 router.post("/task/create", protect, createTask);
 router.get("/tasks", protect, getTasks);
 router.get("/task/:task_id", protect, getTask);
+router.patch("/task/:task_id", protect, updateTask);
+router.delete("/task/:task_id", protect, deleteTask);
 
 export default router;
