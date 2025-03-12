85f4861fee855c636e1a5a10de2f1ae45b3cb840
Asad Bin Saber <mdasad.binsaber@gmail.com>
2025-03-12 16:56:25 +0600

    Auth system: Reset password template added

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import { registerUser, loginUser, logoutUser, getUser, updateUser, userLoginStatus } from '../controllers/userController.js';
-import { verifyEmail, verifyUser } from '../controllers/userController.js';
+import { verifyEmail, verifyUser, forgotPassword } from '../controllers/userController.js';
 import { deleteUser, getAllUsers } from '../controllers/adminController.js';
 import { protect, adminMiddleware, creatorMiddleware, varifiedMiddleware } from '../middleware/authMiddleware.js';
 
@@ -27,5 +27,8 @@ router.post("/verify-email", protect, verifyEmail);
 
 // verify user --> email verification
 router.post("/verify-user/:verificationToken", verifyUser);
+
+// forgot password
+router.post("/forgot-password", forgotPassword);
 
 export default router;
